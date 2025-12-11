aed53c12345b95f46d1e94da70d8772602ac7e35
Sachin-nsk <sachinkumarnsk20090@gmail.com>
2025-12-11 22:35:08 +0530

    Added simple spring boot endpoint -hello

---
+++ day1/src/main/java/com/bajaj/day1/HelloController.java
@@ -0,0 +1,12 @@
+package com.bajaj.day1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello! I have completed Day 1.";
+    }
+}
